d1f1a481f45d79cae99be63d46894c401ef4f8f3
Surapee <suwan.srp@gmail.com>
2023-12-16 16:45:24 +0700

    Create project.js module to export project object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("test")
+// import { isToday, isThisWeek } from 'date-fns';
+
+// // Assuming you have a Date object named currentDate
+// const currentDate = new Date();
+
+// // Check if currentDate is today
+// if (isThisWeek(currentDate)) {
+//   console.log('The date is thisWeek!');
+// } else {
+//   console.log('The date is not today.');
+// }
