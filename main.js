7836d609fd19d486ceadaa746b3167c711b512cc
Surapee <suwan.srp@gmail.com>
2023-12-16 16:48:54 +0700

    Update project.js and re-install date-fns

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("test")
+// import { isToday, isThisWeek } from 'date-fns';
+
+// // Assuming you have a Date object named currentDate
+// const currentDate = new Date();
+
+// // Check if currentDate is today
+// if (isThisWeek(currentDate)) {
+//   console.log('The date is thisWeek!');
+// } else {
+//   console.log('The date is not today.');
+// }
+
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbl90b2RvX2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJ0ZXN0XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLFlBQVksc0JBQXNCOztBQUVsQztBQUNBOztBQUVBO0FBQ0E7QUFDQTtBQUNBLEtBQUs7QUFDTDtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbl90b2RvX2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHsgaXNUb2RheSwgaXNUaGlzV2VlayB9IGZyb20gJ2RhdGUtZm5zJztcblxuLy8gLy8gQXNzdW1pbmcgeW91IGhhdmUgYSBEYXRlIG9iamVjdCBuYW1lZCBjdXJyZW50RGF0ZVxuLy8gY29uc3QgY3VycmVudERhdGUgPSBuZXcgRGF0ZSgpO1xuXG4vLyAvLyBDaGVjayBpZiBjdXJyZW50RGF0ZSBpcyB0b2RheVxuLy8gaWYgKGlzVGhpc1dlZWsoY3VycmVudERhdGUpKSB7XG4vLyAgIGNvbnNvbGUubG9nKCdUaGUgZGF0ZSBpcyB0aGlzV2VlayEnKTtcbi8vIH0gZWxzZSB7XG4vLyAgIGNvbnNvbGUubG9nKCdUaGUgZGF0ZSBpcyBub3QgdG9kYXkuJyk7XG4vLyB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
