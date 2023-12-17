a91b463ab281dda08b4cdd35ff46b3b0f388b54a
Surapee <suwan.srp@gmail.com>
2023-12-17 15:49:51 +0700

    Minimize code in storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,31 +1,18 @@
-import Project from './project'
-import Task from './task'
-import ProjectContainer from './projectContainer'
+/*
+- Web Storage API, specifically localStorage and sessionStorage, stores data as strings.
+  To store and retrieve complex data types like objects, JSON is often used.
+- JSON (JavaScript Object Notation) is a common and convenient format for representing
+  complex data structures in a string form
+*/
 
 export default class Storage {
+  // ProjectContainer CRUD
   static saveProjectContainer(data) {
-    localStorage.setItem('projectContainer', JSON.stringify(data))
+    localStorage.setItem('projectContainer', JSON.stringify(data)) // JSON = JSON.stringify(Js object)
   }
 
   static getProjectContainer() {
-    const projectContainer = Object.assign(
-        new ProjectContainer(),
-        JSON.parse(localStorage.getItem('projectContainer'))
-    )
-
-    projectContainer.setProjectList(
-        projectContainer
-        .getProjectList()
-        .map((project) => Object.assign(new Project(), project))
-    )
-
-    projectContainer
-        .getProjectList()
-        .forEach((project) => project.setTaskList(
-            project.getTaskList().map((task) => Object.assign(new Task(), task))
-            )
-      )
-
+    const projectContainer = JSON.parse(localStorage.getItem('projectContainer'));
     return projectContainer
   }
 
@@ -41,6 +28,19 @@ export default class Storage {
     Storage.saveProjectContainer(projectContainer)
   }
 
+  static updateTodayProject() {
+    const projectContainer = Storage.getProjectContainer()
+    projectContainer.updateTodayProject()
+    Storage.saveProjectContainer(projectContainer)
+  }
+
+  static updateNext7DayProject() {
+    const projectContainer = Storage.getProjectContainer()
+    projectContainer.updateNext7DayProject()
+    Storage.saveProjectContainer(projectContainer)
+  }
+
+  // Project CRUD
   static addTask(projectName, task) {
     const projectContainer = Storage.getProjectContainer()
     projectContainer.getProject(projectName).addTask(task)
@@ -53,6 +53,7 @@ export default class Storage {
     Storage.saveProjectContainer(projectContainer)
   }
 
+  // Task CRUD
   static renameTask(projectName, taskName, newTaskName) {
     const projectContainer = Storage.getProjectContainer()
     projectContainer.getProject(projectName).getTask(taskName).setName(newTaskName)
@@ -65,15 +66,4 @@ export default class Storage {
     Storage.saveProjectContainer(projectContainer)
   }
 
-  static updateTodayProject() {
-    const projectContainer = Storage.getProjectContainer()
-    projectContainer.updateTodayProject()
-    Storage.saveProjectContainer(projectContainer)
-  }
-
-  static updateNext7DayProject() {
-    const projectContainer = Storage.getProjectContainer()
-    projectContainer.updateNext7DayProject()
-    Storage.saveProjectContainer(projectContainer)
-  }
 }
